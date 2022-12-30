2f83a4480cf038e7724c1af8c49dd9c442a2ad1c
realstealthninja <realstealthninja@gmail.com>
2022-12-30 05:53:49 +0530

    feat: addding card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
