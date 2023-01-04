be46eaa29407eb201ecd35f0980e0d0ee5a007ac
realstealthninja <realstealthninja@gmail.com>
2023-01-04 21:24:36 +0530

    fix: projects route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "about", component: AboutComponent},
+  {path: "projects", component: ProjectsComponent},
   {path: "**", component: PageNotFoundComponent}
 ];
 
