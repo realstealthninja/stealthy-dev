4a415b9b7cbbfacbe0c2b164f2fa8bd99cf37537
realstealthninja <realstealthninja@gmail.com>
2023-01-05 19:31:39 +0530

    fix: add contact as a route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { LinksComponent } from './projects/links.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "about", component: AboutComponent},
-  {path: "projects", component: ProjectsComponent},
-  {path: "**", component: PageNotFoundComponent}
+  { path: "", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "contact", component: LinksComponent }
+  { path: "**", component: PageNotFoundComponent }
 ];
 
 @NgModule({
