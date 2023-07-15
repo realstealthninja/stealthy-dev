0e936c486c93e5586c201d9d3b9aaa8543d548ed
realstealthninja <realstealthninja@gmail.com>
2023-07-15 15:29:21 +0530

    fix: blogs html

---
+++ src/app/blogs/blogs.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogs',
+  templateUrl: './blogs.component.html',
+  styleUrls: ['./blogs.component.css']
+})
+
+
+export class BlogsComponent {
+  blogs = [
+    "dev.html",
+    "langs.html"
+  ]
+  
+}
