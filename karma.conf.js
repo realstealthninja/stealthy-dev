702c86b9b61669bc9ec04e640ca24a714d1f70ce
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2023-07-16 19:09:31 +0530

    fix: firefox

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   config.set({
     browsers: ['Chrome','FirefoxHeadless'],
     customLaunchers: {
-      'FirefoxHeadless': {
+      FirefoxHeadless: {
         base: 'Firefox',
         flags: [
           '-headless',
