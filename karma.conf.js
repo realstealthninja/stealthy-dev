076b9f341640f093c044a9871f62bd1ab6216f74
realstealthninja <realstealthninja@gmail.com>
2023-11-15 19:04:46 +0530

    fix: add comma to karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-firefox-launcher')
     ],
@@ -28,7 +28,7 @@ module.exports = function (config) {
     customLaunchers: {
       FirefoxHeadless: {
         base: 'Firefox',
-        flags: [ '-headless' ],
+        flags: ['-headless'],
       },
     },
     singleRun: false,
