b98a0418e359c1d279b4a4edde3ab10e5567b048
realstealthninja <realstealthninja@gmail.com>
2023-11-19 00:27:21 +0530

    fix: add text area and expand the blog

--- src/app/blogs/my-time-at-gbot/my-time-at-gbot.component.ts
+++ src/app/blogs/my-time-at-gbot/my-time-at-gbot.component.ts
@@ -1,12 +1,4 @@
 import { Component } from '@angular/core';
-import hljs from 'highlight.js/lib/core';
-import arduino from 'highlight.js/lib/languages/arduino';
-hljs.registerLanguage('arduino', arduino);
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-  hljs.highlightAll();
-});
 
 @Component({
   selector: 'app-my-time-at-gbot',
