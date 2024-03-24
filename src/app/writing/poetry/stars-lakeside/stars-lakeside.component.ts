0db3c30c3eeadbf94f85408f272129b085d48d5c
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-03-24 18:05:03 +0530

    feat: add an incomplete poem

---
+++ src/app/writing/poetry/stars-lakeside/stars-lakeside.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stars-lakeside',
+  templateUrl: './stars-lakeside.component.html',
+  styleUrls: ['./stars-lakeside.component.css']
+})
+export class StarsLakesideComponent {
+
+}
