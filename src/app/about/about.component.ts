a5021e6c4fa494c84eb31faffd8ffbb1410cb188
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-08-21 20:40:53 +0530

    fix: add date

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,4 +5,10 @@ import { Component } from "@angular/core";
   templateUrl: "./about.component.html",
   styleUrls: ["./about.component.css"],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  date = new Date(Date.now());
+
+  age: number = this.date.getFullYear() - 2007;
+
+
+}
